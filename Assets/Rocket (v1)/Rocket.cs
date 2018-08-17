97b174702835c0cb2b35c76f4c4c2136a93fa4da
Slone <jbslon01@louisville.edu>
2018-08-17 15:14:53 -0400

    Movement and First Level

---
+++ Assets/Rocket (v1)/Rocket.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 1000f;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+        Thrust();
+        Sound();
+    }
+
+    private void Rotate()
+    {
+
+        rigidBody.freezeRotation = true;  // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;  // resume physics control of rotation
+    }
+
+    private void Sound()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly");
+                break;
+            default:
+                print("Do Nothing");
+                break;
+        }
+    }
+}
