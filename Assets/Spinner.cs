7d7fc4358c258c9bf4d888c808c42b8efa52722f
Slone <jbslon01@louisville.edu>
2018-08-24 12:31:33 -0400

    New Levels
    Including Dizzy

---
+++ Assets/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    [SerializeField] float velocity = 90f;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.forward, Time.deltaTime * velocity);
+    }
+}
